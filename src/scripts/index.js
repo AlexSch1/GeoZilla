e32ce5bffe042b777031a62e631b41d12bbe7637
Aliaksei <aleksejsch@yandex.by>
2019-02-15 19:25:09 +0300

    goeZilla last app today

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -28,34 +28,36 @@ $(document).ready(function () {
   }
 
   // Destore invalid text on form
-  $('.send_request [name="e-mail"], .send_request textarea, .select__wr').on('click', function () {
+  $('.send_request [name="email"], .send_request textarea, .select__wr').on('click', function () {
     $(this).closest('.pop_up__input_wr').find('.pop_up__warm_text_chose').removeClass('pop_up__warm_text_no_choose')
     $(this).closest('.pop_up__input_wr').find('.pop_up__warm_text_valid').removeClass('pop_up__warm_text_no_valid')
   })
 
-  // $('.pop_up__field_file').on('click', function () {
-  //   $(this).closest('.pop_up__file_label').find('.pop_up__warm_text_chose').removeClass('pop_up__warm_text_no_choose')
-  // })
-
   // Falidation form
   $('.btn.pop_up__input_btn').click(function (e) {
+    // -
+  })
+  document.getElementById('ajax-contact-form').addEventListener('submit', function (e) {
+    // let targetThis = e.target
+    let state = true
+    e.preventDefault()
     $('.select__wr').each(function (i) {
       if ($(this).find('input[type="radio"]:checked').length === 0) {
         $(this).closest('.pop_up__input_wr').find('.pop_up__warm_text_chose').addClass('pop_up__warm_text_no_choose')
-        e.preventDefault()
+        state = false
       } else {
         if ($(this).find('input[type="radio"]:checked').hasClass('default')) {
           $(this).closest('.pop_up__input_wr').find('.pop_up__warm_text_chose').addClass('pop_up__warm_text_no_choose')
-          e.preventDefault()
+          state = false
         }
       }
     })
     var trigger = true
-    var _this = $('.send_request [name="e-mail"]')
+    var _this = $('.send_request [name="email"]')
     if ($(_this).val() === '') {
       $(_this).closest('.pop_up__input_wr').find('.pop_up__warm_text_chose').addClass('pop_up__warm_text_no_choose')
       $(_this).closest('.pop_up__input_wr').find('.pop_up__warm_text_valid').removeClass('pop_up__warm_text_no_valid')
-      e.preventDefault()
+      state = false
     } else {
       if (!validate(_this, trigger)) {
         $(_this).closest('.pop_up__input_wr').find('.pop_up__warm_text_valid').addClass('pop_up__warm_text_no_valid')
@@ -64,14 +66,28 @@ $(document).ready(function () {
     }
     if ($('.send_request textarea').val() === '') {
       $('.send_request textarea').closest('.pop_up__input_wr').find('.pop_up__warm_text_chose').addClass('pop_up__warm_text_no_choose')
-      e.preventDefault()
+      state = false
     }
-    // if ($('#fileuploadurl').val() === '') {
-    //   $('.pop_up__input_wr_file .pop_up__warm_text').addClass('pop_up__warm_text_no_choose')
-    //   e.preventDefault()
-    // }
     if (!trigger) return false
-  })
+    if (!state) return false
+    var http = new XMLHttpRequest()
+    var f = this
+    var th = $(this)
+    e.preventDefault()
+    http.open('POST', 'contact.php', true)
+    http.onreadystatechange = function () {
+      if (http.readyState === 4 && http.status === 200) {
+        alert(http.responseText)
+        if (http.responseText.indexOf(f.email.value) === 0) {
+          th.trigger('reset')
+        }
+      }
+    }
+    http.onerror = function () {
+      alert('Ошибка, попробуйте еще раз')
+    }
+    http.send(new FormData(f))
+  }, false)
 
   // Validator form impus
   function validate (_this, trigger) {
@@ -139,9 +155,6 @@ $(document).ready(function () {
       $('#chars2').text(0)
       return
     }
-    // console.log(lengthArea, 'lengthArea')
-    // -
-    // -
     if (e.keyCode === 8) {
       if (lengthArea === 0) {
         console.log('clear')
@@ -149,9 +162,6 @@ $(document).ready(function () {
         return false
       }
     }
-    // -
-    // -
-    // console.log((maxlength - lengthArea))
     if ((maxlength - lengthArea) - 1 < counter) {
       $('.pop_up__warm_counter').show()
       let increm = (maxlength - lengthArea)
@@ -161,8 +171,6 @@ $(document).ready(function () {
       }
       $('#chars2').text(increm)
     }
-    // -
-    // -
     if (e.keyCode === 8) {
       let decrim = (maxlength - lengthArea)
       if (decrim > counter) {
@@ -172,58 +180,6 @@ $(document).ready(function () {
       $('#chars2').text(decrim)
     }
   })
-  // let lengthAreaBefore = 0
-  // let valiCounter = maxlength - counter
-  $('.pop_up__field_areas').keydown(function (e) {
-    let lengthArea = $(this).val().length + 1
-    if (lengthArea >= maxlength) return
-    console.log(lengthArea, 'lengthArea')
-    // console.log(lengthAreaBefore, ' bef')
-    // console.log(lengthArea)
-    if (e.keyCode === 8) {
-      // lengthAreaBefore = lengthArea
-      let decr = (maxlength - lengthArea) + 1
-      if (decr === counter) {
-        // valiCounter = maxlength - counter
-        $('.pop_up__warm_counter').hide()
-        return
-      }
-      $('#chars2').text(decr)
-      return
-    }
-    // let lengthAmo = maxlength - lengthArea
-    // $('#chars').text(lengthAmo)
-    if ((maxlength - lengthArea) <= counter) {
-      // if (lengthAreaBefore < lengthArea) return
-      let decrim = (maxlength - lengthArea)
-      $('.pop_up__warm_counter').show()
-      if (decrim <= 0) {
-        $('#chars2').text(0)
-        return
-      }
-      $('#chars2').text(decrim)
-      // lengthAreaBefore = lengthArea
-      // decrim--
-    } else {
-      // if (valiCounter >= counter) {
-      //   $('.pop_up__warm_counter').hide()
-      //   return
-      // }
-      // valiCounter++
-      // $('#chars2').text(valiCounter)
-    }
-  })
-
-  $('.pop_up__field_areas').keyup(function (e) {
-    let lengthArea2 = $(this).val().length
-    if (e.keyCode === 8) {
-      if (lengthArea2 === 0) {
-        $('.pop_up__warm_counter').hide()
-        return false
-      }
-    }
-  })
-
   function textAreaAdjust (o) {
     o.style.height = '1px'
     var offset = o.offsetHeight - o.clientHeight
@@ -232,15 +188,15 @@ $(document).ready(function () {
     console.log($(o).height())
     if (window.innerWidth >= 767) {
       if ($(o).height() >= 150) {
-        $('.pop_up__input_wr_area .pop_up__name_input').css('opacity', '0')
+        // $('.pop_up__input_wr_area .pop_up__name_input').css('opacity', '0')
       } else {
-        $('.pop_up__input_wr_area .pop_up__name_input').css('opacity', '1')
+        // $('.pop_up__input_wr_area .pop_up__name_input').css('opacity', '1')
       }
     } else {
       if ($(o).height() >= 144) {
-        $('.pop_up__input_wr_area .pop_up__name_input').css('opacity', '0')
+        // $('.pop_up__input_wr_area .pop_up__name_input').css('opacity', '0')
       } else {
-        $('.pop_up__input_wr_area .pop_up__name_input').css('opacity', '1')
+        // $('.pop_up__input_wr_area .pop_up__name_input').css('opacity', '1')
       }
     }
   }
@@ -248,4 +204,50 @@ $(document).ready(function () {
     let EO = this
     textAreaAdjust(EO)
   })
+  // -
+  // .start_free_trial
+  $('.start_free_trial').on('click', function (e) {
+    e.preventDefault()
+    console.log('Ваша платформа: ' + findOS())
+    let nameUserPlatfornOne = findOS()
+    if (nameUserPlatfornOne === 'MacOS') {
+      document.location.href = 'https://itunes.apple.com/app/id981856216?_branch_match_id=623206276219064180'
+    } else {
+      let nameUserPlatform = getPlatform()
+      if ((nameUserPlatform === 'iPhone') || (nameUserPlatform === 'iPad') || (nameUserPlatform === 'iPod')) {
+        document.location.href = 'https://itunes.apple.com/app/id981856216?_branch_match_id=623206276219064180'
+      } else {
+        document.location.href = 'https://play.google.com/store/apps/details?id=com.geozilla.family&_branch_match_id=623206276219064180'
+      }
+    }
+  })
+  var userDeviceArray = [
+    {device: 'Android', platform: /Android/},
+    {device: 'iPhone', platform: /iPhone/},
+    {device: 'iPad', platform: /iPad/},
+    {device: 'Symbian', platform: /Symbian/},
+    {device: 'Windows Phone', platform: /Windows Phone/},
+    {device: 'Tablet OS', platform: /Tablet OS/},
+    {device: 'Linux', platform: /Linux/},
+    {device: 'Windows', platform: /Windows NT/},
+    {device: 'Macintosh', platform: /Macintosh/}
+  ]
+  var platform = navigator.userAgent
+  function getPlatform () {
+    for (var i in userDeviceArray) {
+      if (userDeviceArray[i].platform.test(platform)) {
+        return userDeviceArray[i].device
+      }
+    }
+    return 'Неизвестная платформа!' + platform
+  }
+
+  function findOS () {
+    var curOS = 'Not Nmaed...'
+    if (navigator.appVersion.indexOf('Win') !== -1) curOS = 'Windows'
+    if (navigator.appVersion.indexOf('Mac') !== -1) curOS = 'MacOS'
+    if (navigator.appVersion.indexOf('X11') !== -1) curOS = 'UNIX'
+    if (navigator.appVersion.indexOf('Linux') !== -1) curOS = 'Linux'
+    return curOS
+  }
 })
